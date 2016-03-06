a1019ddc62960ef152a29e8ab30e4e36d61293ce
jnels <s0ckeyeus@yahoo.com>
2016-03-05 21:55:53 -0500

    Improved responsiveness.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
+function adjustAboutUsHeight() {
+	var $phyllis = $("#phyllis");
+	var $lindsay = $("#lindsay");
+	$("#phyllis").attr("height","");
+	$("#lindsay").attr("height","");
+	var phyllisHeight = $phyllis.css("height");
+	var lindsayHeight = $lindsay.css("height");
+	
+	if (phyllisHeight < lindsayHeight) {
+		$phyllis.css("height", lindsayHeight);
+	} else if (phyllisHeight > lindsayHeight) {
+		$lindsay.css("height", phyllisHeight);
+	}
+}
+
 $(window).on("click resize scroll", function() {
 	$(".navbar-collapse.in").removeClass("in");
+});
+
+$(window).on("load resize", function() {
+	var docWidth = $(document).width();
+	if (docWidth >= 768) {
+		adjustAboutUsHeight();
+	}
 });
