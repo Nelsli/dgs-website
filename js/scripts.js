e92bc413f3ab9c929e2a56fbe54951fc221ac005
jnels <s0ckeyeus@yahoo.com>
2016-08-02 17:27:51 -0400

    Fixed collapsing nav cut off. Fixed nav at 1080p.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,73 @@
+// Shrinking sticky header /////////////////////////
+
+var maxHeight;
+var minHeight;
+
+function getHeaderHeight() {
+	maxHeight = $(".navbar").css("max-height");
+	maxHeight = maxHeight.replace("px", "");
+	
+	minHeight = $("nav.navbar").css("min-height");
+	minHeight = minHeight.replace("px", "");
+}
+
+function sizeHeader() {
+	getHeaderHeight();
+	//Sizes header
+	var scrollTop = $(document).scrollTop();
+	var calcHeight = maxHeight - scrollTop;
+			
+	$(".shrink").css("height", calcHeight);
+	
+	//Chevron visible/invisible	
+	if (calcHeight < 280) {
+		$(".chevron-top").hide();
+	} else {
+		$(".chevron-top").show();
+	}
+	
+	//Chevron opacity
+	var chevronOpacity = 1.5 - (maxHeight - calcHeight)/100;
+	$(".chevron-top").css("opacity", chevronOpacity);
+}
+
+//Set to scroll to top on refresh
+$(window).on('beforeunload', function () {
+	$(window).scrollTop(0);
+});
+
+$(window).on("load resize", function() {
+		sizeHeader();
+		sizeAboutPanels();
+});
+
+// Resizes header and text upon scroll
+$(window).scroll(function() {
+	sizeHeader();	
+});
+
+//Smooth scrolling
+$(function () {
+	$("a[href*='#']:not([href='#'])").click(function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+});
+
+
+
+//Page scripts //////////////////////////////////
+
 //Resize about us profiles to match > 768px
-$(window).on("load resize", function () {
+function sizeAboutPanels() {
 	$(".about-wrap").height("auto");
 	var docWidth = $(document).width();
 
@@ -11,12 +79,13 @@ $(window).on("load resize", function () {
 	} else {
 		$(".about-wrap").height("auto");
 	}
-});
+}
 
 //Removes nav dropdown
 $(window).on("click resize scroll", function () {
 	$(".navbar-collapse.in").removeClass("in");
 });
+
 
 //Testimonial slider
 var testimonials = $(".testimonial").clone();
